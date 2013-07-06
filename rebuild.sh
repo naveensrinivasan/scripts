58e8855eae1b4e8b643c40bc03dbd75536b423b5
Naveen <naveensrinivasan@yahoo.com>
2013-07-06 14:08:31 -0400

    Script to rebuild dev box

---
+++ rebuild.sh
@@ -0,0 +1,9 @@
+# dev tools
+sudo apt-get install git
+sudo apt-get install vim-gnome
+
+# kde
+sudo add-apt-repository ppa:kubuntu-ppa/backports
+sudo apt-get update
+sudo apt-get install kde-standard kubuntu-desktop
+
