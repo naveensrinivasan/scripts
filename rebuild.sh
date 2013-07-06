fac227edea2a4632e1f31587df8665a88ea6e63f
Naveen <naveensrinivasan@yahoo.com>
2013-07-06 17:47:05 -0400

    For getting the ip address of the system to ssh

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,16 @@
 # dev tools
-sudo apt-get install git
-sudo apt-get install vim-gnome
+sudo apt-get install git gitk vim-gnome openjdk-7-jdk curl leiningen
 
 # kde
 sudo add-apt-repository ppa:kubuntu-ppa/backports
 sudo apt-get update
 sudo apt-get install kde-standard kubuntu-desktop
+
+#zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+
+#change default editor
+sudo update-alternatives --config editor
+
 
