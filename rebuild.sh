4c727320516fbfb4ef3f808c387a7346f029c138
Naveen <naveensrinivasan@yahoo.com>
2013-07-06 18:45:34 -0400

    Ruby installation

--- rebuild.sh
+++ rebuild.sh
@@ -1,18 +1,44 @@
+#!/usr/bin/sh
+
+#BECOME SUPER USER#
+ 
+sudo -i
+ 
+#add sublime to repository#
+ 
 # dev tools
-sudo apt-get install git gitk vim-gnome openjdk-7-jdk curl leiningen xclip
+apt-get install git gitk vim-gnome openjdk-7-jdk curl leiningen xclip
 
 # kde
-sudo add-apt-repository ppa:kubuntu-ppa/backports
-sudo apt-get update
-sudo apt-get install kde-standard kubuntu-desktop
+add-apt-repository ppa:kubuntu-ppa/backports
+apt-get update
+apt-get install kde-standard kubuntu-desktop
 
 #zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
 
 #change default editor
-sudo update-alternatives --config editor
+update-alternatives --config editor
 
 #setup keys 
 ssh-keygen -t rsa -C "naveensinivasan@yahoo.com"
 xclip -sel clip < ~/.ssh/id_rsa.pub
+
+
+#sublime text
+add-apt-repository ppa:webupd8team/sublime-text-2
+#update#
+apt-get update
+#install#
+apt-get install sublime-text
+
+#ruby
+apt-get install build-essential openssl libreadline6 libreadline6-dev \
+curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev \
+sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake \
+libtool bison subversion nodejs
+
+curl -L https://get.rvm.io | bash -s stable --ruby
+
+echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> ~/.bashrc
